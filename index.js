e5d91d3d42c3c2425cb76461eb7faf3973932c99
ermi00 <miguelgonzalez.280803@gmail.com>
2025-08-05 19:22:21 -0600

    Responsive terminado, efectos de sonido, estetica visual

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let btn0 = document.getElementById("btn$0") 
+let btn0 = document.getElementById("btn$0");
 let btn50cc = document.getElementById("btn$50cc");
 let btn1 = document.getElementById("btn$1");
 let btn2 = document.getElementById("btn$2");
@@ -10,16 +10,27 @@ let btn100 = document.getElementById("btn$100");
 let btn200 = document.getElementById("btn$200");
 let btn500 = document.getElementById("btn$500");
 let btn1000 = document.getElementById("btn$1000");
+let btnIniciar = document.getElementById("btnIniciar");
 let tuCuenta = document.getElementById("tuCuenta");
 let ptotalCuenta = document.getElementById("totalCuenta");
 let pteDan = document.getElementById("teDan");
 let pMensajes = document.getElementById("pMensajes");
 let divBilletes = document.getElementById("divBilletes");
 let divMonedas = document.getElementById("divMonedas");
-let iconoCambiar = document.getElementById("iconoCambiar")
-let contador = 0
+let iconoCambiar = document.getElementById("iconoCambiar");
+let btnReinciar = document.getElementById("btnReiniciar");
+let selecionarSonido = new Audio("assets/selecionar.wav");
+let errorSonido = new Audio("assets/error.wav");
+let alternarSonido = new Audio("assets/alternar.wav");
+let deshacerSonido = new Audio("assets/deshacer.wav");
+let ganarSonido = new Audio("assets/ganar.wav");
+let cancionDeFondo  = new Audio("assets/cancionDeFondo.mp3")
 
-let totalCuenta = Math.floor(Math.random() * 100);
+let divInicio = document.getElementById("divInicio");
+let divJuego = document.getElementById("divJuego");
+
+let contador = 0;
+let totalCuenta = Math.floor(Math.random() * 1000);
 let cambioDado;
 
 let billete10 = {
@@ -52,6 +63,11 @@ let billete500 = {
   valor: 500 - totalCuenta,
 };
 
+let billete1000 = {
+  nombre: "1000",
+  valor: 1000 - totalCuenta,
+};
+
 let billetes = [
   billete10,
   billete20,
@@ -59,6 +75,7 @@ let billetes = [
   billete100,
   billete200,
   billete500,
+  billete1000,
 ];
 
 let teDan = parseInt(queBilleteUsar(billetes));
@@ -69,7 +86,8 @@ function meterMoneda(boton) {
   let total = lista.reduce((a, b) => a + b, 0);
   tuCuenta.innerHTML = `Tu cuenta $ ${total.toFixed(2)}`;
   cambioDado = total;
-  console.log(cambioDado)
+  let sonido = selecionarSonido.cloneNode();
+  sonido.play();
 }
 
 function deshacerMovimiento() {
@@ -77,7 +95,8 @@ function deshacerMovimiento() {
   let nuevoTotal = lista.reduce((a, b) => a + b, 0);
   tuCuenta.innerHTML = `Tu cuenta $ ${nuevoTotal.toFixed(2)}`;
   cambioDado = nuevoTotal;
-  console.log(cambioDado)
+  let sonido = deshacerSonido.cloneNode();
+  sonido.play();
 }
 
 function queBilleteUsar(billetes) {
@@ -91,6 +110,8 @@ function queBilleteUsar(billetes) {
 }
 
 function entregarCambio() {
+  let sonido = errorSonido.cloneNode();
+  let sonidoGanar = ganarSonido.cloneNode();
   resultado = teDan - totalCuenta;
   resultadoUsuario = cambioDado;
 
@@ -99,29 +120,49 @@ function entregarCambio() {
     pMensajes.style.fontSize = "15px";
     pMensajes.style.padding = "15px";
     pMensajes.style.fontFamily = "Sixtyfour";
-    pMensajes.innerHTML = "Bien hecho";
+    pMensajes.innerHTML = "Buen trabajo";
+    sonidoGanar.play();
   } else {
     pMensajes.style.backgroundColor = "black";
     pMensajes.style.fontSize = "15px";
     pMensajes.style.padding = "15px";
     pMensajes.style.fontFamily = "Sixtyfour Convergence";
     pMensajes.innerHTML = "Intenta de nuevo, el cambio esta mal.";
+    sonido.play();
   }
 }
 
 function cambiarBotones() {
+  let sonido = alternarSonido.cloneNode();
   contador++;
-
+  sonido.play();
   if (contador % 2 === 0) {
     divBilletes.style.display = "flex";
-    divMonedas.style.display = "none"
-    iconoCambiar.src = "/assets/monedaIcon.png"
+    divMonedas.style.display = "none";
+    iconoCambiar.src = "/assets/monedaIcon.png";
   } else {
     divBilletes.style.display = "none";
-    divMonedas.style.display = "flex"
-    iconoCambiar.src = "/assets/billeteIcon.png"
+    divMonedas.style.display = "flex";
+    iconoCambiar.src = "/assets/billeteIcon.png";
   }
 }
+
+function reiniciarJuego() {
+  document.location.reload();
+}
+
+function iniciarJuego() {
+  divInicio.style.display = "none";
+  divJuego.style.display = "block";
+  let sonido = selecionarSonido.cloneNode();
+  sonido.play();
+}
+
+function tocarMusica(){
+  cancionDeFondo.play()
+}
+
+divJuego.style.display = "none";
 
 ptotalCuenta.innerHTML = `Total $${totalCuenta} <br> Recibes $${teDan}`;
 // pteDan.innerHTML = `Recibes $${teDan}`;
